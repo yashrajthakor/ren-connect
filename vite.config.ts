d7bdb29b8d2b5b9b79a297a209215afa9e6e3749
Yashraj <56244480+yashrajthakor@users.noreply.github.com>
2026-05-02 14:31:02 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  preview: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
